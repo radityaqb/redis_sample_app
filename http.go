2d10fce1780d8a8fb72891d9eda28821c02fca5b
Fath Imtiaz <fath.imtiaz@tokopedia.com>
2020-05-08 15:27:51 +0700

    init handler

--- http.go
+++ http.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func initHandler() {
 	http.HandleFunc("/ping", handlePing)
 	// TODO: Adding more APIs
 
+	router := httprouter.New()
+
+	router.GET("/get_prize", handleGetPrize)
+
 	fmt.Println("serving in 8181 ")
 	http.ListenAndServe(":8181", nil)
 }
 
 func handlePing(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
+func handleGetPrize(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
 	w.Write([]byte("pong"))
 }
